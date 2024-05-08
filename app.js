e747c8103af043defa9fde76f79bfd12984f66da
ocmodi21 <ocmodi2117@gmail.com>
2024-05-09 02:53:15 +0530

    refactor: app.js file

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const { logger } = require("./middlewares/logger");
 require("dotenv").config();
 
 const userRoutes = require("./routes/user.routes");
 const recruiterRoutes = require("./routes/recruiter.routes");
+const candidateRoutes = require("./routes/candidate.routes");
 const app = express();
 
 app.use(cors());
+app.use(logger);
 app.use(express.json());
 
-app.use("/api/user", userRoutes);
-app.use("/api/recruiter", recruiterRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/recruiter", recruiterRoutes);
+app.use("/api/v1/candidate", candidateRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

20553555fed843aa3a8a111024b9ad3b25a5e2fe
ocmodi21 <ocmodi2117@gmail.com>
2024-05-12 09:51:48 +0530

    refactor: prettier code

--- routes/recruiter.routes.js
+++ routes/recruiter.routes.js
@@ -1,19 +1,39 @@
 const express = require("express");
 const recruiterControllers = require("../controllers/recruiter.controllers");
 const { authUser } = require("../middlewares/jwtAuth.middlewares");
-const { verifiedUser } = require("../middlewares/verifiedUser.middleware")
+const { verifiedUser } = require("../middlewares/verifiedUser.middleware");
 const router = express.Router();
 
 router.post("/job", authUser, verifiedUser, recruiterControllers.postJob);
 
 router.get("/jobs", authUser, verifiedUser, recruiterControllers.listJobs);
 
-router.get("/job/:jobId/applications", authUser, verifiedUser, recruiterControllers.listApplications);
+router.get(
+  "/job/:jobId/applications",
+  authUser,
+  verifiedUser,
+  recruiterControllers.listApplications
+);
 
-router.patch("/job/:jobId/closeJob", authUser, verifiedUser, recruiterControllers.closeJob);
+router.patch(
+  "/job/:jobId/closeJob",
+  authUser,
+  verifiedUser,
+  recruiterControllers.closeJob
+);
 
-router.get("/job/:jobId/applications/:applicationId", authUser, verifiedUser, recruiterControllers.getApplication)
+router.get(
+  "/job/:jobId/applications/:applicationId",
+  authUser,
+  verifiedUser,
+  recruiterControllers.getApplication
+);
 
-router.patch("/job/:jobId/applications/:applicationId", authUser, verifiedUser, recruiterControllers.updateApplication)
+router.patch(
+  "/job/:jobId/applications/:applicationId",
+  authUser,
+  verifiedUser,
+  recruiterControllers.updateApplication
+);
 
 module.exports = router;
